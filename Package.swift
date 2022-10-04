ce04813a712f918d1deb30346d6cb8ce60ee63cf
DosZlmnv <doszlmnv@gmail.com>
2022-10-04 10:53:02 +0600

    platform added

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
  let package = Package(
      name: "CardIO",
+     platforms: [
+         .iOS("11")
+     ],
      products: [
          .library(
              name: "CardIO",
